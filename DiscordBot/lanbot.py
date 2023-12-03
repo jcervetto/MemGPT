cc4f0d566007e055a1ab7db0c5dcd66888770ee1
jcervetto <96144760+jcervetto@users.noreply.github.com>
2023-12-03 14:31:17 -0800

    Lanbot time management

--- DiscordBot/lanbot.py
+++ DiscordBot/lanbot.py
@@ -1,18 +1,21 @@
 import os
 import dotenv
 import discord
-from localAssistant import LocalAssistant  # Import the updated LocalAssistant class
+import requests
+from datetime import datetime
+import pytz  # Import the pytz module for time zone handling
+from localAssistant import LocalAssistant
 
 dotenv.load_dotenv()
 
 bot_token = os.getenv("LAN_TOKEN")
 intents = discord.Intents.default()
-intents.message_content = True  # Ensure the bot can access the content of messages
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 # Initialize the LocalAssistant instance
-assistant = LocalAssistant("ws://localhost:8282", "agent_4")  # Replace with your WebSocket URI and agent name
+assistant = LocalAssistant("ws://localhost:8282", "agent_6")
 
 @client.event
 async def on_ready():
@@ -24,31 +27,29 @@ async def on_message(message):
         return
 
     if client.user is not None and client.user.mentioned_in(message):
-        # Capture the username of the message sender
+        # Fetch current time
+        response = requests.get("http://worldtimeapi.org/api/timezone/America/Los_Angeles")
+        current_time_data = response.json()
+        current_time_str = current_time_data["datetime"]
+        current_time = datetime.fromisoformat(current_time_str)
+        formatted_current_time = current_time.strftime("%A, %B %d %H:%M")
+
         username = message.author.display_name
-
-        # Process the message content, ignoring the mention
         user_message = message.content.replace(f"@{client.user.name}", "")
-
-        # Send the message to the LocalAssistant
-        responses = await assistant.send_and_receive_message(f"{username} : {user_message}")
+        responses = await assistant.send_and_receive_message(f"Time: The current time in Vancouver is {formatted_current_time}  \n {username} : {user_message} ")
         
         if responses:
             for response in responses:
-                # Format the response to include the username
                 formatted_response = f"{response}"
                 await send_split_messages(message.channel, formatted_response)
         else:
             await message.channel.send("No response received or an error occurred.")
     else:
-        # Ignore other messages
         return
 
 async def send_split_messages(channel, message, char_limit=2000):
-    """Sends a message in chunks if it's longer than the char_limit."""
     for i in range(0, len(message), char_limit):
         chunk = message[i:i+char_limit]
         await channel.send(chunk)
 
 client.run(bot_token)
-
